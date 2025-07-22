78de8525d2acd9eb2ab177f77ae935e5deb44c61
Sven <sven.perkmann@gmail.com>
2025-07-22 20:37:13 +0200

    better title font

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import { Game } from "~/Game";
 function App() {
   return (
     <div className="w-screen h-screen flex justify-center items-center flex-col gap-8">
-      <h1 className="text-4xl font-bold">
-        Theorore's Epic Quest for all the Cakes
+      <h1 className="text-4xl font-bold font-pacifico">
+        Theodore's Epic Quest for all the Cakes
       </h1>
 
       <Game />
